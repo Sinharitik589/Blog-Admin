9cc75b638f293a571aadcb0e02cf1dd091c1cf77
Sinharitik589 <sinharitik18112835@gmail.com>
2021-03-01 22:35:03 +0530

    first commit for netlify

--- src/Pages.js/Navigation.js
+++ src/Pages.js/Navigation.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import { Route } from 'react-router'
+import { Route, Switch } from 'react-router'
 import Blog from './Blog'
 import Blogs from './Blogs'
 import EditForm from './EditForm'
 import Form from './Form'
 
 
-
 const Navigation = () => {
 
     return (
         <Container style={{ marginTop: 100 }}>
-            <Route exact path="/" component={Blogs} />
-            <Route path="/add" component={Form} />
-            <Route path="/edit:index" component={EditForm} />
-            <Route path="/blog:id" component={Blog} />
+            <Switch>
+                <Route exact path="" component={Blogs} />
+                <Route path="add" component={Form} />
+                <Route path="edit:index" component={EditForm} />
+                <Route path="blog:id" component={Blog} />
+            </Switch>
         </Container>
     )
 }
